32c370cb452799d013ee7fb35e562a6455eb098b
fury786786S <165811939+fury786786S@users.noreply.github.com>
2024-08-14 20:53:15 +0530

    add

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
         <div className="items" >
           {skills.map((data) => (<>
 
-            <div className="item" key={data.id}>
+            <div className="item col-6 col-md-4 col-lg-3 mb-4" key={data.id}>
               <img src={`/assets/${data.imageSrc}`} alt="" srcset="" />
               <h3>{data.title}</h3>
             </div>
